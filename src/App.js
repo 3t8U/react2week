9cb5a5455dda4953f30b138d243f3d25a9d3b2bd
Student <example@example.com>
2020-02-14 09:00:54 -0800

    add foundational styling

--- src/App.js
+++ src/App.js
@@ -4,28 +4,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import GoogleFontLoader from 'react-google-font-loader';
 import YouTube from 'react-youtube';
-import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          /html/body/div[4]/main/div[1]/section/div/div/div/div[1]
-        </p>
-
-
-
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
